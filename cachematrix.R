1dcbbd7a2be432615d4cb308ba13a816d8d03dad
mubeenahmed0010 <ahmedmubeen0010@gmail.com>
2025-09-06 17:03:36 +0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+r_code <- c(
+  "makeCacheMatrix <- function(x = matrix()) {",
+  "  inv <- NULL",
+  "  set <- function(y) {",
+  "    x <<- y",
+  "    inv <<- NULL",
+  "  }",
+  "  get <- function() x",
+  "  setinverse <- function(inverse) inv <<- inverse",
+  "  getinverse <- function() inv",
+  "  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)",
+  "}",
+  "",
+  "cacheSolve <- function(x, ...) {",
+  "  inv <- x$getinverse()",
+  "  if (!is.null(inv)) {",
+  "    message(\"getting cached data\")",
+  "    return(inv)",
+  "  }",
+  "  mat <- x$get()",
+  "  if (!is.matrix(mat)) stop(\"The stored object is not a matrix.\")",
+  "  if (nrow(mat) != ncol(mat)) stop(\"Matrix must be square to compute its inverse.\")",
+  "  inv <- tryCatch({",
+  "    solve(mat, ...)",
+  "  }, error = function(e) {",
+  "    stop(\"Matrix inversion failed: \", e$message)",
+  "  })",
+  "  x$setinverse(inv)",
+  "  inv",
+  "}"
+)
+writeLines(r_code, "cachematrix.R")
+getwd()               # shows the folder where cachematrix.R was written
+file.exists("cachematrix.R")
 
-## Write a short comment describing this function
+source("cachematrix.R")
 
-makeCacheMatrix <- function(x = matrix()) {
+# quick test
+m <- matrix(c(2,1,1,2), 2, 2)
+cm <- makeCacheMatrix(m)
 
-}
+# first call computes inverse
+cacheSolve(cm)
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# second call should use cached result and print "getting cached data"
+cacheSolve(cm)
